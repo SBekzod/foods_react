8b13d4c00ebb2e6e3f4ad32145f6d3bde3c92d36
Bekzod Sanakulov <bekzodsanakulov89@gmail.com>
2020-03-31 04:48:37 +0900

    waste

--- src/components/MainCom.js
+++ src/components/MainCom.js
@@ -5,13 +5,13 @@ import Footer from './FooterCom';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Staff from './StaffCom';
 
-const Main = (prop) => {
+const Main = (props) => {
     return (
         <div>
             <Header />
             <Switch>
                 <Route path="/staff" component={Staff} />
-                <Route path="/menu" component={() => <Menu dishlar={prop.dishlar} />} />
+                <Route path="/menu" component={() => <Menu dishlar={props.dishlar} />} />
                 <Redirect to="/menu" />
             </Switch>
             <Footer />
