38fb6e04c47aaf9735e46115a2a9bda0ed6489db
Bekzod Sanakulov <bekzodsanakulov89@gmail.com>
2020-04-04 17:35:54 +0900

    REDUX STORE COMMITTING

--- src/components/MainCom.js
+++ src/components/MainCom.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react';
 import Header from './HeaderComponent';
 import Menu from './MenuComponents';
 import Footer from './FooterCom';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import Staff from './StaffCom';
 import Forms from './FillCom';
 import Contact from './ContactCom';
 import Contact2 from './ContactCom2'
 import Home from './HomeCom';
 import Itempresent from './ItemPresCom';
-import { DISHES } from '../shared/dishes';
+import { connect } from 'react-redux';
+
+
+const mapStateToProps = state => {
+    return {
+        dishlar: state.dishlar
+    }
+}
 
 
 class Main extends Component {
@@ -17,15 +24,12 @@ class Main extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            dishlar: DISHES
-        }
     }
 
     render() {
         const ChosenItem = ({ match }) => {
             return (
-                <Itempresent dish={this.state.dishlar.filter((ele) => ele.id === parseInt(match.params.id, 10))[0]} />
+                <Itempresent dish={this.props.dishlar.filter((ele) => ele.id === parseInt(match.params.id, 10))[0]} />
             );
         }
 
@@ -35,8 +39,8 @@ class Main extends Component {
                 <Switch>
                     <Route path="/staff" component={Staff} />
                     <Route path="/menu/:id" component={ChosenItem} />
-                    <Route exact path="/menu" component={() => <Menu dishlar={this.state.dishlar} />} />
-                    <Route path="/home" component={() => <Home dish={this.state.dishlar[0]} />} />
+                    <Route exact path="/menu" component={() => <Menu dishlar={this.props.dishlar} />} />
+                    <Route path="/home" component={() => <Home dish={this.props.dishlar[0]} />} />
                     <Route path="/form" component={Forms} />
                     <Route path="/contact" component={Contact} />
                     <Route path="/contact2" component={Contact2} />
@@ -48,8 +52,6 @@ class Main extends Component {
 
     }
 
-
-
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
---
+++ src/redux/reducer.js
@@ -0,0 +1,9 @@
+import { DISHES } from '../shared/dishes';
+
+export const initialState = {
+    dishlar: DISHES,
+};
+
+export const Reducer = (state = initialState, action) => {
+    return state;
+};
