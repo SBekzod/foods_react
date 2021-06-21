4d1836f6d5cac1018016d526c4cdf531a1f5de28
Martin <martin@comets.kr>
2021-06-21 21:31:32 +0900

    feat: avoiding null value data unexist

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -1,17 +1,17 @@
 import * as ActionTypes from './ActionTypes';
 import { act } from 'react-dom/test-utils';
 
-export const Rcomments = (state = { comments: null, errMess: null, errMess2: null }, action) => {
+export const Rcomments = (state = { comments: [], errMess: null, errMess2: null }, action) => {
     switch (action.type) {
         case ActionTypes.COMMENTS_ADDED:
             return { ...state, comments: action.payload, errMess: null, errMess2: null }
         case ActionTypes.COMMENTS_FAILED:
-            return { ...state, comments: null, errMess: action.payload, errMess2: null }
+            return { ...state, comments: [], errMess: action.payload, errMess2: null }
         case ActionTypes.ADD_COMMENT:
             const newComment = action.payload;
             return { ...state, comments: state.comments.concat(newComment), errMess: null, errMess2: null }
         case ActionTypes.ADD_COMMENT_FAILED:
-            return { ...state, comments: null, errMess: null, errMess2: action.payload }
+            return { ...state, comments: [], errMess: null, errMess2: action.payload }
         default:
             return state;
     }
--- src/redux/dishComments.js
+++ src/redux/dishComments.js
@@ -1,16 +1,16 @@
 import * as ActionTypes from './ActionTypes';
 
-export const RdishComment = (state = { dishComments: null, errMess: null, errMessTwo: null }, action) => {
+export const RdishComment = (state = { dishComments: [], errMess: null, errMessTwo: null }, action) => {
     switch (action.type) {
         case ActionTypes.DISH_COMMENTS_ADDED:
             return { ...state, dishComments: action.payload, errMess: null, errMessTwo: null }
         case ActionTypes.DISH_COMMENTS_FAILED:
-            return { ...state, dishComments: null, errMess: action.payload, errMessTwo: null }
+            return { ...state, dishComments: [], errMess: action.payload, errMessTwo: null }
         case ActionTypes.NEW_DISH_COMMENT_SUCCESS:
             const newDishComment = action.payload;
             return { ...state, dishComments: state.dishComments.concat(newDishComment), errMess: null, errMessTwo: null }
         case ActionTypes.NEW_DISH_COMMENT_FAILED:
-            return { ...state, dishComments: null, errMess: null, errMessTwo: action.payload }
+            return { ...state, dishComments: [], errMess: null, errMessTwo: action.payload }
         default:
             return state
     }
